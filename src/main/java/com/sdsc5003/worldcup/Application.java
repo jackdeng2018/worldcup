3d1502b162913a935f6330f68f3546eabf366300
denghan <dhdenghan@163.com>
2022-12-14 12:47:21 +0800

    --update

--- src/main/java/com/sdsc5003/worldcup/Application.java
+++ src/main/java/com/sdsc5003/worldcup/Application.java
@@ -1,13 +1,8 @@
 package com.sdsc5003.worldcup;
 
-import org.brickred.socialauth.SocialAuthConfig;
-import org.brickred.socialauth.SocialAuthManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 
 @SpringBootApplication(scanBasePackages = "com.sdsc5003.worldcup")
 @EnableCaching
@@ -15,13 +10,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
-    public SocialAuthManager socialAuthManager(SocialAuthConfig socialAuthConfig) throws Exception {
-        SocialAuthManager socialAuthManager = new SocialAuthManager();
-        socialAuthManager.setSocialAuthConfig(socialAuthConfig);
-        return socialAuthManager;
     }
 }
